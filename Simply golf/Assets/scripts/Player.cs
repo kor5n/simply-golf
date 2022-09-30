00ead708ea731b21ba76a8a825572c0cd5432e68
kor5n <jegor.kotov@gmail.com>
2022-09-30 19:19:50 +0200

    made saving position system

--- Simply golf/Assets/scripts/Player.cs
+++ Simply golf/Assets/scripts/Player.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [Header("Movement")]
-    public static float maxSpeed = 400000;
-    public static float minSpeed = 10000;
+    public static float maxSpeed = 400000f;
+    public static float minSpeed = 44074.6f;
     public static float speed;
     private Rigidbody rb;
-    
+
+    private Vector3 PastPos;
     
     public GameObject powerBar;
 
@@ -23,17 +24,44 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //StartShooting();
+        
         Movement();
     }
     void Movement()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-                
+            SavePos();
+            rb.isKinematic = false;
             rb.AddForce(transform.forward * speed * Time.deltaTime);       
             score++;
+            
                 
         }
     }
+    void SavePos()
+    {
+        PastPos = transform.position;
+    }
+    void LoadPos()
+    {
+        rb.isKinematic = true;
+        transform.position = PastPos;
+    }
+    void StopMoving()
+    {
+        if(rb.velocity.magnitude <= 0.1)
+        {
+            rb.isKinematic = true;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Water")
+        {
+            LoadPos();
+        }
+    }
+
+
 }
